b9df6424bde6d9c7d0d9231fcff5719092122e4b
niveaabreu <niveaadl@al.insper.edu.br>
2023-09-10 11:25:06 -0300

    Detecting Collisions with Collectibles completo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -33,7 +33,10 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        other.gameObject.SetActive(false);
+        if(other.gameObject.CompareTag("PickUp")){
+             other.gameObject.SetActive(false);
+        }
+       
     }
 
 }
